eb81a706096bf6899431619b07e24bc15dbb5b3f
manishkumarroy <manishkumar222mani@gmail.com>
2025-11-13 10:05:19 +0530

    Modify Service principal

--- azure-infrastructure/service-principal.tf
+++ azure-infrastructure/service-principal.tf
@@ -8,20 +8,22 @@ data "azurerm_role_definition" "builtin" {
 data "azurerm_client_config" "current" {
 }
 
-resource "azurerm_role_assignment" "rbac" {
-  scope                = data.azurerm_subscription.primary.id
-  role_definition_name = data.azurerm_role_definition.builtin.name
-  principal_id         = data.azurerm_client_config.current.object_id
-}
-
 resource "azuread_application" "git" {
   display_name = "Github Actions"
+}
+
+resource "azuread_service_principal" "sp" {
+  client_id = azuread_application.git.client_id
 }
 
 resource "azuread_application_password" "password" {
   application_id = azuread_application.git.id
 }
 
-resource "azuread_service_principal" "sp" {
-  client_id = azuread_application.git.client_id
+resource "azurerm_role_assignment" "rbac" {
+  scope                = data.azurerm_subscription.primary.id
+  role_definition_name = data.azurerm_role_definition.builtin.name
+  principal_id         = azuread_service_principal.sp.object_id
 }
+
+
