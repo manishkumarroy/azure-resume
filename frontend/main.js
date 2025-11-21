6e500eed6a293832e2a65db06181c9a14672f9ad
manishkumarroy <manishkumar222mani@gmail.com>
2025-11-21 18:05:52 +0530

    Fix fetch URL for GetResumeCounter function

--- frontend/main.js
+++ frontend/main.js
@@ -6,7 +6,7 @@ const localFunctionApi = 'http://localhost:7071/api/GetResumeCounter';
 
 const getVisitorCount = () =>{
     let count = 30
-    fetch(functionApi).then(response => {
+    fetch(functionApiUrl).then(response => {
         return response.json()
     }).then(response => {
         console.log("website called function API: ")
